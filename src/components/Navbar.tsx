408eb16366b5b19f0f548118af82afb8e37f83d8
fahad <fahadmahmud225@gmail.com>
2024-03-14 05:35:04 +0600

    added a logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,10 @@
 import { useMenuBarStore } from "@/state/MenuBarState";
 import { CloudSunRain, Wind } from "lucide-react";
 import { Lato } from "next/font/google";
+import Image from "next/image";
 import Link from "next/link";
 import React, { HTMLAttributes } from "react";
+import Logo from "@/../public/weather-logo.svg";
 
 // fonts
 const lato = Lato({ weight: "400", subsets: ["latin"] });
@@ -20,11 +22,12 @@ const Navbar: React.FC<NavbarProps> = ({ ...props }) => {
       className="flex flex-col items-center bg-[var(--primary-bg-color)] h-full px-1 py-3 lg:px-3 lg:py-5 rounded-2xl md:rounded-[var(--primary-border-radius)]"
     >
       {/* logo */}
-      <abbr title="Weather Pulse">
-        <div className="bg-[var(--secondary-bg-color)] aspect-square p-1 rounded-xl">
-          <Wind className="w-8 h-8" />
-        </div>
-      </abbr>
+      <div
+        title="Weather Pulse"
+        className="bg-[var(--secondary-bg-color)] aspect-square p-1 rounded-xl"
+      >
+        <Image alt="Logo" src={Logo} className="w-12 h-12" />
+      </div>
 
       <div
         className={`${lato.className} text-[var(--gray-text-color)] text-sm space-y-7 mt-10`}
